23eafbcb4aa45842d3fc39193984eb8b6bf22f6a
youichiro <cinnamon416@gmail.com>
2018-08-27 01:26:13 +0900

    urlの変更

--- message/urls.py
+++ message/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.MessageListView.as_view(), name='message_view'),
-    path('message_form/', views.MessageCreateView.as_view(), name='message_form'),
+    path('form/', views.MessageCreateView.as_view(), name='message_form'),
     path('<int:id>/reply_form/', views.ReplyCreateView.as_view(), name='reply_form'),
 ]
