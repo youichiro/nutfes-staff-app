539b10150e9ec018e52323f954cd9c8787c4c9a3
youichiro <cinnamon416@gmail.com>
2018-08-27 21:49:20 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOGIN_URL = 'login'
