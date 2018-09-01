7fb1476c2384d13e06a19d3d8f8cc7e0bc5463ce
youichiro <cinnamon416@gmail.com>
2018-09-01 21:53:22 +0900

    現在の日付に応じて表示するシフトを変える

--- shift/views.py
+++ shift/views.py
@@ -5,7 +5,36 @@ from .models import Shift
 from .scripts.const import time_to_time
 
 
-DEFAULT_SHIFT_ID = 1  # シフトの指定がない場合の表示するシフト
+def get_default_shift_id():
+    now = datetime.datetime.now()
+    date = now.date()
+    if date == datetime.date(2018, 9, 14):
+        return 1
+    elif date == datetime.date(2018, 9, 15):
+        return 3
+    elif date == datetime.date(2018, 9, 16):
+        return 5
+    elif date == datetime.date(2018, 9, 17):
+        return 7
+    else:
+        return 3
+
+
+def get_time_now():
+    now = datetime.datetime.now()
+    if now.minute < 30:
+        time_now = str(now.hour).rjust(2, '0') + ':00'
+    else:
+        time_now = str(now.hour).rjust(2, '0') + ':30'
+    return time_now
+
+
+def get_times_and_tasks(query):
+    attr_list = [[time_to_time[k], v] for k, v in query.__dict__.items() if k[0] == 't']
+    times = [attr[0] for attr in attr_list]
+    tasks = [attr[1] for attr in attr_list]
+    time_flags = [1 if time == get_time_now() else 0 for time in times]
+    return times, tasks, time_flags
 
 
 def prepare_task_list(tasks, flags):
@@ -35,29 +64,12 @@ def get_personal_shifts(user):
     return [shift1, shift2, shift3, shift4, shift5, shift6, shift7]
 
 
-def get_time_now():
-    now = datetime.datetime.now()
-    if now.minute < 30:
-        time_now = str(now.hour).rjust(2, '0') + ':00'
-    else:
-        time_now = str(now.hour).rjust(2, '0') + ':30'
-    return time_now
-
-
-def get_times_and_tasks(query):
-    attr_list = [[time_to_time[k], v] for k, v in query.__dict__.items() if k[0] == 't']
-    times = [attr[0] for attr in attr_list]
-    tasks = [attr[1] for attr in attr_list]
-    time_flags = [1 if time == get_time_now() else 0 for time in times]
-    return times, tasks, time_flags
-
-
 class ShiftListView(LoginRequiredMixin, ListView):
     model = Shift
     template_name = 'shift/shift_list.html'
 
     def get_queryset(self):
-        queryset = Shift.objects.filter(shift_id=DEFAULT_SHIFT_ID)
+        queryset = Shift.objects.filter(shift_id=get_default_shift_id())
         shift_id_request = self.request.GET.get('shift_id')
         if shift_id_request:
             queryset = Shift.objects.filter(shift_id=shift_id_request)
@@ -104,7 +116,7 @@ class MyShiftView(LoginRequiredMixin, TemplateView):
         user = self.request.user
         if not Shift.objects.filter(user=user).exists():
             return
-        shift = Shift.objects.filter(user=user, shift_id=DEFAULT_SHIFT_ID).first()
+        shift = Shift.objects.filter(user=user, shift_id=get_default_shift_id()).first()
         times, tasks, flags = get_times_and_tasks(shift)
         context['shift'] = shift
         context['times'] = zip(times, flags)
